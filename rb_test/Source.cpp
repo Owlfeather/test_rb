e5a65a68d50ba102c2ce6009e8a698f07fb5da2d
Полина Мартынюк <polinka1106@yandex.ru>
2019-09-21 15:42:44 +0300

    2

--- rb_test/Source.cpp
+++ rb_test/Source.cpp
@@ -6,6 +6,6 @@ using namespace std;
 void main() {
 	cout << "hello!" << endl;
 	cout << "we are in developer!" << endl;
-	//cout << "oops, we need rollback!" << endl;
+	cout << "oops, we need rollback!" << endl;
 	system("pause");
 }
